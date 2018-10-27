5a6126af62fecad604b521fc53b99f34ed123f12
Chet Corcos <ccorcos@gmail.com>
2018-10-26 17:57:23 -0700

    Serve files.

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,7 @@
 const { app, protocol, BrowserWindow } = require("electron")
 const request = require("request")
+const fs = require("fs")
+const path = require("path")
 
 // TODO: persist jar with touch-cookie-store, perhaps using electron-store
 const cookieJar = request.jar()
@@ -13,28 +15,52 @@ app.on("ready", async function() {
 		customProtocol,
 		(req, callback) => {
 			const httpUrl = req.url.replace(customProtocol, "http")
-			console.log(req)
+			console.log(httpUrl)
 			console.log("\n")
-			const stream = request({
-				url: httpUrl,
-				headers: req.headers,
-				method: req.method,
-				jar: cookieJar,
-				body:
-					req.uploadData &&
-					req.uploadData[0] &&
-					req.uploadData[0].bytes.toString("utf8"),
-			})
-
-			stream.on("response", resp => {
-				console.log(httpUrl, resp.statusCode, resp.headers)
-				console.log("\n")
+			if (httpUrl.endsWith("/")) {
+				const fileStream = fs.createReadStream(
+					path.resolve(path.join(__dirname, "../server/index.html"))
+				)
 				callback({
-					statusCode: resp.statusCode,
-					headers: resp.headers,
-					data: resp,
+					statusCode: 200,
+					headers: {
+						"content-type": "text/html",
+					},
+					data: fileStream,
 				})
-			})
+			} else if (httpUrl.endsWith("/app.js")) {
+				const fileStream = fs.createReadStream(
+					path.resolve(path.join(__dirname, "../server/app.js"))
+				)
+				callback({
+					statusCode: 200,
+					headers: {
+						"content-type": "application/javascript",
+					},
+					data: fileStream,
+				})
+			} else {
+				const stream = request({
+					url: httpUrl,
+					headers: req.headers,
+					method: req.method,
+					jar: cookieJar,
+					body:
+						req.uploadData &&
+						req.uploadData[0] &&
+						req.uploadData[0].bytes.toString("utf8"),
+				})
+
+				stream.on("response", resp => {
+					console.log(httpUrl, resp.statusCode, resp.headers)
+					console.log("\n")
+					callback({
+						statusCode: resp.statusCode,
+						headers: resp.headers,
+						data: resp,
+					})
+				})
+			}
 		},
 		error => {
 			if (error) {
