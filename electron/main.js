4e940c4d46d2a255321ff193912dde38fcaf6f46
Chet Corcos <ccorcos@gmail.com>
2018-10-31 15:00:21 -0700

    Comment

--- electron/main.js
+++ electron/main.js
@@ -24,6 +24,7 @@ app.on("ready", async function() {
 			console.log(req)
 			console.log("\n")
 
+			// TODO: might be better to push buffers into the request stream?
 			let body = undefined
 			if (req.uploadData) {
 				body = req.uploadData
